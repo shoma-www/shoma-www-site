f82812dead08e0c2bd681ee6075793b95b8390a0
shoma-www <shumai.1215.2018@gmail.com>
2021-04-06 01:48:22 +0900

    remove files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
 import { useDeno } from 'framework/react'
 import React from 'react'
-import Logo from '~/components/logo.tsx'
-import useCounter from '~/lib/useCounter.ts'
 
 export default function Home() {
-  const [count, isSyncing, increase, decrease] = useCounter()
   const version = useDeno(() => Deno.version.deno)
 
   return (
@@ -12,7 +9,6 @@ export default function Home() {
       <head>
         <title>Hello World - Aleph.js</title>
       </head>
-      <p className="logo"><Logo /></p>
       <h1>Welcome to use <strong>Aleph.js</strong>!</h1>
       <p className="links">
         <a href="https://alephjs.org" target="_blank">Website</a>
@@ -23,17 +19,6 @@ export default function Home() {
         <span></span>
         <a href="https://github.com/alephjs/aleph.js" target="_blank">Github</a>
       </p>
-      <div className="counter">
-        <span>Counter:</span>
-        {isSyncing && (
-          <em>...</em>
-        )}
-        {!isSyncing && (
-          <strong>{count}</strong>
-        )}
-        <button onClick={decrease}>-</button>
-        <button onClick={increase}>+</button>
-      </div>
       <p className="copyinfo">Built by Aleph.js in Deno {version}</p>
     </div>
   )
