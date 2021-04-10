c2b1c17b2e63b4c824dce086b17f1dadda027307
shoma-www <shumai.1215.2018@gmail.com>
2021-04-10 19:10:00 +0900

    add pointer cursor

--- components/organisms/toggle.tsx
+++ components/organisms/toggle.tsx
@@ -4,7 +4,10 @@ export default function Toggle(
   { open, onClick }: { open: boolean; onClick?(e: React.MouseEvent): void },
 ) {
   return (
-    <div className="fixed z-10 right-10 bottom-20" onClick={onClick}>
+    <div
+      className="fixed z-10 right-10 bottom-20 cursor-pointer"
+      onClick={onClick}
+    >
       {!open && <span className="animate-pulse text-4xl">丸</span>}
       {open && <span className="animate-pulse text-4xl">罰</span>}
     </div>
