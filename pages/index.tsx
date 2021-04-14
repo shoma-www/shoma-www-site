4651f04bfdbfdb71353eadd30f0492b42aa8b633
shoma-www <shumai.1215.2018@gmail.com>
2021-04-15 05:02:56 +0900

    add favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,12 +23,38 @@ export default function Home() {
         />
         <meta
           property="og:image"
-          content={`${BASE_URL}/public/images/og-image.png`}
+          content={`${BASE_URL}/images/og-image.png`}
         />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@w0GumZcZGwOHfBB" />
         <meta property="og:site_name" content="Shoma's Home" />
         <meta property="og:locale" content="ja_JP" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/images/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/images/favicon/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
 
       <Navbar open={open} onClick={() => setOpen(!open)} />
