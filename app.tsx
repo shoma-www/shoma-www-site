a3fe9621ff2d577cd7bfb68f84fe6ad392145b3f
shoma-www <shumai.1215.2018@gmail.com>
2021-04-22 21:11:30 +0900

    ermove gtm

--- app.tsx
+++ app.tsx
@@ -7,12 +7,6 @@ export default function App(
   // deno-lint-ignore no-explicit-any
   { Page, pageProps }: { Page: ComponentType<any>; pageProps: any },
 ) {
-  const GA_TRACKING_ID = "GTM-5X9CK2S";
-  const gtmScript =
-    `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GA_TRACKING_ID}');`;
-  const gtmFrame =
-    `<iframe src="https://www.googletagmanager.com/ns.html?id=${GA_TRACKING_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
-
   return (
     <main>
       <head>
@@ -54,13 +48,6 @@ export default function App(
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
           rel="stylesheet"
         />
-        <script dangerouslySetInnerHTML={{ __html: gtmScript }} />
-        <link
-          rel="alternate"
-          href={`${BASE_URL}/rss/article.rss`}
-          type="application/rss+xml"
-          title="RSS"
-        />
         <link
           rel="alternate"
           href={`${BASE_URL}/rss/article.rss`}
@@ -69,7 +56,6 @@ export default function App(
         />
       </head>
       <body>
-        <noscript dangerouslySetInnerHTML={{ __html: gtmFrame }} />
         <Page {...pageProps} />
       </body>
     </main>
